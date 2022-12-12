9da6ab072aec93ab0f5cb13490826edfd28d1a96
rickyl3 <113804998+rickyl3@users.noreply.github.com>
2022-12-12 14:01:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Part 1
+// function initialize() {
+//     numberOutput = document.getElementById("numoutput");
+//     number = 0;
+// }
+// function modifyNumber(modifier) {
+//     number += modifier;
+//     display();
+// }
+// function display() {
+//     numberOutput.innerHTML = number;
+// }
+// function getRandomInteger(lower, upper) {
+//     var multiplier = upper - (lower - 1);
+//     var rnd = parseInt(Math.random() * multiplier) + lower;
+
+//     return rnd;
+// }
+
